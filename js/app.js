096e2593babdf47b10f7a5b2639659270b42aaee
NishanDas <nishandas880@gmail.com>
2022-02-17 18:11:37 +0600

    all error declerat

--- js/app.js
+++ js/app.js
@@ -2,13 +2,23 @@
  function getIncomeTotal(){
     const totalIncomeText=document.getElementById('total-income').value;
     const totalIncome=parseFloat(totalIncomeText);
+    if(isNaN(totalIncome)==true || totalIncome <0){
+      document.getElementById('income-alert').innerText='**please enter a valid number?**';
+      return false;
+    }
+   else{
     return totalIncome;
+   }
  };
 /*==== total expenses handling ====*/
  function getExpensesInput(expenses){
     //  find expenses
     const expensesText=document.getElementById(expenses +'-expenses').value;
     const expensesNumber=parseFloat(expensesText);
+    if(isNaN(expensesNumber)==true || expensesNumber <0){
+      document.getElementById(expenses+'-alert').innerText='**please enter a valid number?**';
+      return false;
+   }
     return expensesNumber;
     
  };
@@ -20,6 +30,11 @@
    const totalCostId=document.getElementById('total-cost');
    const totalCostText=totalCostId.innerText;
    const totalExpenses=foodExpense + rentExpense + clothesExpense;
+   const inTotalIncome=getIncomeTotal();
+   if(totalExpenses > inTotalIncome){
+    document.getElementById('total-cost').innerText='**Much more expenses**';
+    return false;
+  }
    totalCostId.innerText=totalExpenses;
    return totalExpenses;
 
@@ -29,7 +44,7 @@ function getTotalBlance(){
   const totalBalanceId=document.getElementById('balance');
     const totalBalanceText=totalBalanceId.innerText;
     const inTotalIncome =getIncomeTotal();
-    const inTotalCost=getTotalCost()
+    const inTotalCost=getTotalCost();
     const totalBalance=inTotalIncome -inTotalCost;
     totalBalanceId.innerText=totalBalance;
      return totalBalance;
@@ -38,6 +53,10 @@ function getTotalBlance(){
 function getSavePersent(){
   const savePersentInput=document.getElementById('save-percent').value;
   const savePersent=parseFloat(savePersentInput);
+  if(isNaN(savePersent)==true || savePersent <0){
+    document.getElementById('persent-alert').innerText='**please enter a valid number?**';
+    return false;
+ }
   return savePersent;
 }
 /*==== get save amount  ====*/
@@ -47,6 +66,11 @@ function getSaveAmount(){
  const savePersent=getSavePersent();
  const inTotalIncome =getIncomeTotal();
  const saveAmount=(inTotalIncome * savePersent) / 100;
+ const inTotalBlance=getTotalBlance();
+ if(saveAmount > inTotalBlance){
+  document.getElementById('save-amount').innerText='**Much save amount**';
+  return false;
+ }
  saveAmountId.innerText=saveAmount;
  return saveAmount;
 }
@@ -65,10 +89,13 @@ function getRemainingBlance(){
   /*====calculate-btn event handling ====*/
 document.getElementById('calculate-btn').addEventListener('click',function(){
   // getIncomeTotal fuction call  
+  
    getIncomeTotal();
   //getTotalCost fuction call  
+   debugger;
    getTotalCost();
   //getTotalBlance fuction call  
+ 
     getTotalBlance();
 });
 /* ==== save-btn event handling ====*/
